39fa496959c10b371029fd4ec99432f7c278df73
Dan Galdi <dmgaldi@upenn.edu>
2022-04-04 09:40:54 -0400

    Upgrade major version of jersey

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,5 +1,5 @@
 val jackson = "2.12.2" // FasterXML Jackson version
-val jersey  = "2.33"   // Jersey/JaxRS version
+val jersey  = "3.0.4"   // Jersey/JaxRS version
 val junit   = "5.7.1"  // JUnit version
 val log4j   = "2.16.0" // Log4J version
 
