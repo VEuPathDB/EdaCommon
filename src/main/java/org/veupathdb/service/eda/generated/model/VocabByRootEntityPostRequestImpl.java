bc7460eaf95a7e6123beceaaa895f9eff2783cb7
Dan Galdi <dmgaldi@upenn.edu>
2023-08-02 13:33:29 -0400

    Fix model

--- src/main/java/org/veupathdb/service/eda/generated/model/VocabByRootEntityPostRequestImpl.java
+++ src/main/java/org/veupathdb/service/eda/generated/model/VocabByRootEntityPostRequestImpl.java
@@ -1,22 +1,9 @@
 package org.veupathdb.service.eda.generated.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder("studyEntity")
+@JsonPropertyOrder
 public class VocabByRootEntityPostRequestImpl implements VocabByRootEntityPostRequest {
-  @JsonProperty("studyEntity")
-  private String studyEntity;
-
-  @JsonProperty("studyEntity")
-  public String getStudyEntity() {
-    return this.studyEntity;
-  }
-
-  @JsonProperty("studyEntity")
-  public void setStudyEntity(String studyEntity) {
-    this.studyEntity = studyEntity;
-  }
 }
