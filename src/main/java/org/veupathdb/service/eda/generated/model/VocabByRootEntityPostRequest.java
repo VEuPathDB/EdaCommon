8ee1041cd75513aae37d082e94fba3c2806905a1
Dan Galdi <dmgaldi@upenn.edu>
2023-08-02 13:33:29 -0400

    Fix model

--- src/main/java/org/veupathdb/service/eda/generated/model/VocabByRootEntityPostRequest.java
+++ src/main/java/org/veupathdb/service/eda/generated/model/VocabByRootEntityPostRequest.java
@@ -1,15 +1,9 @@
 package org.veupathdb.service.eda.generated.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonDeserialize(
     as = VocabByRootEntityPostRequestImpl.class
 )
 public interface VocabByRootEntityPostRequest {
-  @JsonProperty("studyEntity")
-  String getStudyEntity();
-
-  @JsonProperty("studyEntity")
-  void setStudyEntity(String studyEntity);
 }
