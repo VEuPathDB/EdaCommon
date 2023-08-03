88d5505cc330c765e7ff3e3e800b11c6fca5f3db
Dan Galdi <dmgaldi@upenn.edu>
2023-08-03 15:42:34 -0400

    Address comments

--- src/main/java/org/veupathdb/service/eda/generated/model/VocabByRootEntityPostRequest.java
+++
@@ -1,9 +0,0 @@
-package org.veupathdb.service.eda.generated.model;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-@JsonDeserialize(
-    as = VocabByRootEntityPostRequestImpl.class
-)
-public interface VocabByRootEntityPostRequest {
-}
