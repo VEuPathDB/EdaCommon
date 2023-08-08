2e558bc7573700a6d8a39d0a183f5585695ccdd4
Dan Galdi <dmgaldi@gmail.com>
2023-08-08 09:42:16 -0400

    String collections 10.x (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ containerBuild {
     group = "org.veupathdb.service.eda"
 
     // Project Version
-    version = "10.7.0"
+    version = "10.7.1"
 
     // Project Root Package
     projectPackage = "org.veupathdb.service.eda"
