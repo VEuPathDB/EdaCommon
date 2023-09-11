77bcaee2785986a4b591948aa6626bc680f42707
Dan Galdi <dmgaldi@upenn.edu>
2023-09-11 13:23:05 -0400

    Iterate version for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ containerBuild {
     group = "org.veupathdb.service.eda"
 
     // Project Version
-    version = "11.4.0"
+    version = "11.4.1"
 
     // Project Root Package
     projectPackage = "org.veupathdb.service.eda"
