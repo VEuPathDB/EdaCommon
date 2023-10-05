e01e354fbd2b98a01e2bf91ec128320d794741ef
Danielle Callan <dcallan@upenn.edu>
2023-10-05 14:10:59 -0400

    beta5

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ containerBuild {
     group = "org.veupathdb.service.eda"
 
     // Project Version
-    version = "11.5.0-beta4"
+    version = "11.5.0-beta5"
 
     // Project Root Package
     projectPackage = "org.veupathdb.service.eda"
